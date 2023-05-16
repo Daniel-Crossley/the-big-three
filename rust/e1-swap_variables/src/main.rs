377ad44a1f5b7377f437833b2bcac4e805b67c7c
Daniel-Crossley <daniel-crossley@aapt.net.au>
2023-05-16 21:47:36 +1000

    Updated e1 rust

--- rust/e1-swap_variables/src/main.rs
+++ rust/e1-swap_variables/src/main.rs
@@ -1,20 +1,16 @@
 use std::io;
 use std::io::Write;
 
-fn flush() {
-    io::stdout().flush().expect("Failed")
-}
-
 fn main() {
     let mut a = String::new();
     let mut b = String::new();
 
     print!("a = ");
-    flush();
+    io::stdout().flush().expect("Failed");
     io::stdin().read_line(&mut a).expect("Failed to read line");
 
     print!("b = ");
-    flush();
+    io::stdout().flush().expect("Failed");
     io::stdin().read_line(&mut b).expect("Failed to read line");
 
     let a = a.trim();
